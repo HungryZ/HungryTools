9c830d74fa1524e2f57b3e69697619ce29f71f0d
Zhanghaichuan <924320752@qq.com>
2019-05-27 11:43:41 +0800

    添加新宏和二倍三倍眼睛图

--- HungryTools/Classes/Macro/UtilsMacro.h
+++ HungryTools/Classes/Macro/UtilsMacro.h
@@ -40,7 +40,10 @@
 #define RGBA(r, g, b, a)                    [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
 
 #define HexColor(hex)                       [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0 \
-green:((float)((hex & 0xFF00) >> 8))/255.0 \
-blue:((float)(hex & 0xFF))/255.0 alpha:1.0]
+                                                            green:((float)((hex & 0xFF00) >> 8))/255.0 \
+                                                             blue:((float)(hex & 0xFF))/255.0 alpha:1.0]
+
+#define Font(x)                             [UIFont systemFontOfSize:(x)]
+
 
 #endif /* UtilsMacro_h */
