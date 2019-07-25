b941030d483976486049c14d9b827042f9fd9fd7
Zhanghaichuan <924320752@qq.com>
2019-07-25 15:29:53 +0800

    1

---
+++ HungryTools/Classes/Category/UILabel+Size.h
@@ -0,0 +1,24 @@
+//
+//  UILabel+Size.h
+//  StrategyPlus
+//
+//  Created by 张海川 on 2019/7/25.
+//  Copyright © 2019 张海川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (Size)
+
+/// 单行文本宽度
+@property (nonatomic, assign, readonly) float textWidth;
+/// 多行普通文本高度
+- (float)textHeightWithWidth:(float)width;
+/// 多行富文本高度（需先赋值 attributedText 属性）
+- (float)attributedTextHeightWithWidth:(float)width;
+
+@end
+
+NS_ASSUME_NONNULL_END
